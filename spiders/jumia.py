0ced1027832b86cb56166d3ed5f1ded301aaace7
asmae-43 <79695140+asmae-43@users.noreply.github.com>
2023-06-02 21:36:37 +0100

    Update jumia.py

--- spiders/jumia.py
+++ spiders/jumia.py
@@ -1,10 +1,22 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+import pandas as pd 
+infoProduct = []
+for page in range(1,50):
+  url = "https://www.jumia.ma/mlp-all-deals/" + "?page=" +str(page)+"#catalog-listing"
+  url = requests.get(url)
+  soup = BeautifulSoup(url.content , 'html.parser')
+  products = soup.find_all('div' , class_ = 'info')
 
-url = 'https://www.jumia.ma/mlp-boutique-officielle-adidas/'
-html_file = requests.get(url).text
-soup = BeautifulSoup(html_file, 'html.parser')
-data_category = 'Health & Beauty/Personal Care/Bath & Bathing Accessories'
-product_titles = soup.find_all('a', class_='core')
-for title in product_titles:
-    print(title.text.strip(' '))
+  for product in products:
+      Name = product.find('h3' , class_="name").text.replace('\n', '')
+      OriginalPrice = product.find('div' , class_= "prc").text.replace('\n', '')
+      #OldPrice = product.find('div', class_= "old").text.replace('\n', '')
+      try:
+        Rating = product.find('div', class_='stars _s').text.replace('\n', '')
+      except:
+        Rating = 'None'
+      infoProduct.append([ Name, OriginalPrice, Rating])
+      #print(infoProduct)
+df = pd.DataFrame(infoProduct)
+df.to_excel('results.xlsx', index=False)
